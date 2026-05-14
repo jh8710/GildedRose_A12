3d5aa4d543e1f85b62b123e67396d49de3e18251
jh8710 <bloodhorn87@gmail.com>
2026-05-14 15:31:28 +0900

    test소스 수정

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,13 +9,13 @@ import com.gildedrose.simulate30Days;
 
 public class GildedRoseTest {
 
-    // @Test
-    // public void foo() {
-    //     Item[] items = new Item[] { new Item("foo", 0, 0) };
-    //     GildedRose app = new GildedRose(items);
-    //     app.updateQuality();
-    //     assertEquals("fixme", app.items[0].name);
-    // }
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
 
 
 
