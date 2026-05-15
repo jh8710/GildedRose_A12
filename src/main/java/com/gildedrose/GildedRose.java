73dc3802bfe574a6b9c76efd85cd58c76b2fc8b8
jh8710 <bloodhorn87@gmail.com>
2026-05-15 10:19:38 +0900

    refactoring 후 주석 제거

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,22 +14,6 @@ class GildedRose {
         this.items = items;
     }
 
-    // public void updateQuality() {
-    // for (Item item : items) {
-    // if(item.name.equals(AGED_BRIE))
-    // new AgedBrieItem(item).updateQuality();
-    // else if(item.name.equals(BACKSTAGE))
-    // new BackStageItem(item).updateQuality();
-    // else if(item.name.equals(SULFURAS))
-    // new SulfurasItem(item).updateQuality();
-    // else if(item.name.equals(CONJURED))
-    // new ConjuredItem(item).updateQuality();
-    // else
-    // new NormalItem(item).updateQuality();
-    // updateSellIn(item);
-    // }
-    // }
-
     private GildedRoseItem getItem(Item item) {
         if (item.name.equals(AGED_BRIE))
             return new AgedBrieItem(item);
@@ -57,117 +41,9 @@ class GildedRose {
         gi.updateQuality();
     }
 
-    // public void updateQuality() {
-    //     for (Item item : items) {
-    //         GildedRoseItem gi = getItem(item);
-    //         gi.updateQuality(); // 다형성!
-    //         updateSellIn(item);
-    //     }
-    // }
-
-    // private void updateAgedBrie(Item item){
-    // if(item.quality < 50) item.quality++;
-    // if(item.sellIn < 1)
-    // if(item.quality < 50) item.quality++;
-    // }
-
-    // private void updateBackstage(Item item){
-    // if(item.quality < 50) item.quality++;
-    // if(item.sellIn<11 && item.quality<50) item.quality++;
-    // if(item.sellIn<6 && item.quality<50) item.quality++;
-    // if(item.sellIn<1) item.quality = 0;
-
-    // }
-    // private void updateSulfuras(Item item){}
-
-    // private void updateNormalItem(Item item){
-    // if(item.quality > 0) item.quality--;
-    // if(item.sellIn<1 && item.quality>0)
-    // item.quality--;
-    // }
     private void updateSellIn(Item item) {
         if (!item.name.equals(SULFURAS))
             item.sellIn--;
     }
 
-    // private void updateConjured(Item item){
-    // if(item.quality > 0) {
-    // if(item.quality - 2 <= 0) item.quality = 0;
-    // else item.quality = item.quality-2;
-    // }
-    // if(item.sellIn<1 && item.quality>0) {
-    // if(item.quality - 2 <= 0) item.quality = 0;
-    // else item.quality = item.quality-2;
-    // }
-    // }
-
-    // public void updateQuality() {
-    // for (int i = 0; i < items.length; i++) {
-    // Item item = items[i];
-    // if (!item.name.equals(AGED_BRIE)
-    // && !item.name.equals(BACKSTAGE)) {
-    // if (item.quality > 0) {
-    // if (!item.name.equals(SULFURAS)) {
-    // if(item.name.equals(CONJURED)){
-    // if(item.quality - 2 <= 0){
-    // item.quality = 0;
-    // }
-    // else
-    // item.quality = iitem.quality - 2;
-    // }
-    // else
-    // item.quality = item.quality - 1;
-    // }
-    // }
-    // } else {
-    // if (item.quality < 50) {
-    // item.quality = item.quality + 1;
-
-    // if (item.name.equals(BACKSTAGE)) {
-    // if (item.sellIn < 11) {
-    // if (item.quality < 50) {
-    // item.quality = item.quality + 1;
-    // }
-    // }
-
-    // if (item.sellIn < 6) {
-    // if (item.quality < 50) {
-    // item.quality = item.quality + 1;
-    // }
-    // }
-    // }
-    // }
-    // }
-
-    // if (!item.name.equals(SULFURAS)) {
-    // item.sellIn = item.sellIn - 1;
-    // }
-
-    // if (item.sellIn < 0) {
-    // if (!item.name.equals(AGED_BRIE)) {
-    // if (!item.name.equals(BACKSTAGE)) {
-    // if (item.quality > 0) {
-    // if (!item.name.equals(SULFURAS)) {
-    // if(item.name.equals(CONJURED)){
-    // if(item.quality - 2 < 0) {
-    // item.quality = 0;
-    // }
-    // else
-    // item.quality = item.quality - 2;
-    // }
-    // else
-    // item.quality = item.quality - 1;
-    // }
-    // }
-    // } else {
-    // item.quality = item.quality - item.quality;
-    // }
-    // } else {
-    // if (item.quality < 50) {
-    // item.quality = item.quality + 1;
-    // }
-    // }
-    // }
-    // }
-    // }
 }
